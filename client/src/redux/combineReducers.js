1c87622793cc9e9943e724800be4919a74b0988e
THE H3RMIT <72027864+PrathamAyush@users.noreply.github.com>
2023-03-26 03:32:22 +0530

    Delete client directory

--- client/src/redux/combineReducers.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux"
-import { cartReducer } from "./cartReducer"
-
-export const combineReducer = combineReducers({ cartReducer: cartReducer });
--- client/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import { combineReducer } from "./combineReducers";
-
-export const store = createStore(
-    combineReducer
-)
